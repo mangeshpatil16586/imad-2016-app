dd1e55aed87bfc0f6238111b580315aaed403e7a
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-09-28 18:42:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one',function(req,res){
-    res.sendFile('Artical one requested  and will be server here');
+    res.send('Artical one requested  and will be server here');
     });
     
 app.get('/article-two',function(req,res){
