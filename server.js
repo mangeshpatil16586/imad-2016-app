f019dacc819cc50e30755f4e5a27c340392fd06e
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-09-28 19:05:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/article-two',function(req,res){
     });
     
 app.get('/article-three',function(req,res){
-    res.send('Artical three requested  and will be server here');
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
     });   
     
     
