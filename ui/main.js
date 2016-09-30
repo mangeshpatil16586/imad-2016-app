ae52aff04903909c4eb6764c7a7d3b90b89e50e0
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-09-30 17:53:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@ element.innerHTML='New value of Main Page';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft + 10;
+    marginLeft=marginLeft + 1;
     img.style.marginLeft=marginLeft+'px';
 }
 
 img.onClick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
    // img.style.marginLeft='100px';
 };
