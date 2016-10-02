183caadbc2e0455235540fa0d579b92862937084
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-10-02 20:26:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,38 +1,8 @@
-/*//change the text
-
-var element=document.getElementById('main_text');
-element.innerHTML='New value of Main Page';
-
-
-//Move the Image
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onClick=function(){
-    var interval=setInterval(moveRight,50);
-   // img.style.marginLeft='100px';
-}; */
 
 var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function(){
-    
-    
-  //Make a request To counter endPoint
-  
-  
-  //Capture the response and store it in a veriable
-  
-  //render the veriable
   counter++;
   document.getElementById('count').innerHTML=counter;
- 
-  
 };
