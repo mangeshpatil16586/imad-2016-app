d736799fa98ee66322e1e9736608d81d8e3cb1d3
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-10-12 20:30:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,25 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_button');
 
 submit.onclick = function(){
-     var names=['name1','name2','name3'];
-     var list='';
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+               var names=request.responsetext;
+               name=JSON.parse(names);
      for(var i=0; i< names.length; i++){
          
          list +='<li>'+names[i]+'</li>';
          
      }
      var ul=document.getElementById('namelist');
-     ul.innerHTML=list;
+     ul.innerHTML=list; 
+            }
+        }
+    };
+    request.open('GET','http://mangeshpatil16586.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+     
 };
