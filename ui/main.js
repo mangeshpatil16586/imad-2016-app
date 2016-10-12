3ad4573310ea8c7195197adc99a017690a52dd54
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-10-12 14:39:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,4 @@
 
-var button = document.getElementById('counter');
-var counter = 0;
-var display= document.getElementById('count');
-
-button.onclick = function(){
-  counter++;
- display.innerHTML=counter.toString();
-};
-
 
 //submit name
 var nameInput=document.getElementById('name');
