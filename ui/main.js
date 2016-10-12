7bd02eab5dd39a6028d0cc20bdd2bf8899f68c92
mangeshpatil16586 <mangesh.patil35@gmail.com>
2016-10-12 14:39:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,8 @@ var submit=document.getElementById('submit_button');
 submit.onclick = function(){
      var names=['name1','name2','name3'];
      var list='';
-     for(var i=0;i<names.length;i++){
+     for(var i=0; i<names.length; i++){
+         
          list +='<li>'+names[i]+'</li>';
          
      }
